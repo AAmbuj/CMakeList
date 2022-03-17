fbdc4d634338e051b58e8a794e0646d1718cab1b
a.kushwaha <ambuj.kushwaha-ext@faurecia.com>
2022-03-17 14:12:13 +0530

    first program

---
+++ CMake2/src/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<< "Hello AMbuj"<<std::endl;
+    std::cout<< "Hello AMbuj"<<std::endl;
+    std::cout<< "Hello AMbuj"<<std::endl;
+    std::cout<< "Hello AMbuj"<<std::endl;
+    std::cout<< "Hello AMbuj"<<std::endl;
+    std::cout<< "Hello AMbuj"<<std::endl;
+    return 0;
+}
